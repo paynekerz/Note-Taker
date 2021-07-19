acc81c6b716d4d5e7abf4293827bf7b70eba671e
Payne Kerz <paynekerz@gmail.com>
2021-07-19 13:56:28 -0700

    initial commit

---
+++ Develop/server.js
@@ -0,0 +1,69 @@
+//using final star wars and tucker solved as a reference
+const path = require("path");
+const fs = require("fs/promises");
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3001;
+const dbPath = path.join(__dirname, "db/db.json");
+
+//parses out the body of the request and gets access to req.body
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+//HOME ROUTES
+//sends html to the respective page
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname + "/public/index.html"));
+  });
+
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname + "/public/notes.html"));
+  });
+
+// GET NOTES
+app.get("/api/notes/", async (req, res) => {
+  //try block to catch errors
+  try{
+    const burrito = await fs.readFile(dbPath, "utf8");
+    res.json(JSON.parse(burrito));
+  }
+  catch (err) {
+    res.status(500).end("GET NOTES ERROR")
+  }
+})
+
+//POST NOTES LOOK INTO THIS LATER
+app.post("/api/notes/", async (req, res) => {
+  //try block to catch errors
+  try {
+    //assigns id to new notes
+    const newNote = req.body
+    newNote.id = req.body.title
+    const oldNotes = await fs.readFile(dbPath, "utf8");
+    let newNotes = [newNote, ...oldNotes];
+    fs.writeFile(dbPath, newNotes);
+    res.json(newNotes);
+  }
+  catch (err){
+    res.status(500).end("POST NOTES ERROR")
+  }
+})
+
+//DELETE NOTES
+app.delete("/api/notes/:id", async (req, res) => {
+  try { 
+  const deletedNote = req.params.id;
+  const storedNotes = await fs.readFile(dbPath, "utf8");
+  let filteredNotes = storedNotes.filter(note => deletedNote !== note.id)
+  fs.writeFile(dbPath, filteredNotes);
+  res.json(filteredNotes);
+  }
+  catch {
+    res.status(500).end("DELETE NOTES ERROR")
+  }
+})
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
